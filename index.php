632e1673023b92ec2f0fa76893dc2ec7375ad761
Dominic Olajire <40606972+lerryjay@users.noreply.github.com>
2022-03-02 14:38:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   $default = 'global';
   $path = explode('/', trim($_SERVER['PHP_SELF'],'/'));
  	
-  define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'].'/'.$path[0]);
+//   define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'].'/'.$path[0]);
+  define('BASE_PATH', '/app'.'/');
   require_once 'GSystem/GCore/index.php';
 ?>
