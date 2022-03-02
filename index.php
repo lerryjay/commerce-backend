1ef4b1a7e1cc01ac098f44f80083fd3305e17c11
Dominic Olajire <40606972+lerryjay@users.noreply.github.com>
2022-03-02 14:39:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
   $path = explode('/', trim($_SERVER['PHP_SELF'],'/'));
  	
 //   define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'].'/'.$path[0]);
-  define('BASE_PATH', '/app'.'/');
+  define('BASE_PATH', '/app');
   require_once 'GSystem/GCore/index.php';
 ?>
