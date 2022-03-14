28ad7c9113fad34c750c1d510ffd81b8e018a439
Dominic Olajire <40606972+lerryjay@users.noreply.github.com>
2022-03-14 16:52:11 +0100

    Delete GSystem directory

--- GSystem/GCore/cli/gc_composer.php
+++
@@ -1,9 +0,0 @@
-<?php
-class GCCliComposer extends CommandController
-{
-  public function run($argv)
-  {
-    array_splice($argv,0,2);
-    echo shell_exec("composer ".implode(' ',$argv));
-  }
-}
