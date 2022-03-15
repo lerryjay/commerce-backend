83da3a499f1895d46d16d3e83c7c2b7da37b6980
Dominic Olajire <40606972+lerryjay@users.noreply.github.com>
2022-03-15 07:28:11 +0100

    Update installapache.sh

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,1 +1,9 @@
-sudo apt install apache2
+#!/bin/bash
+
+if ! pidof apache2 > /dev/null
+then
+        if ! test -f "$FILE"; then
+                sudo apt install apache2
+        fi
+else echo 'Server already intalled and running'
+fi
