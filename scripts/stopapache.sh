04d8d4d03d76d8db3ce8c21572ab70c0a9ed0d2b
Dominic Olajire <40606972+lerryjay@users.noreply.github.com>
2022-03-15 07:33:50 +0100

    Update stopapache.sh

--- scripts/stopapache.sh
+++ scripts/stopapache.sh
@@ -1,1 +1,7 @@
-sudo systemctl stop apache2
+#!/bin/bash
+
+if !pidof apache2 > /dev/null
+then
+  echo 'Server had already been stopped or was not running!'   
+else  sudo systemctl stop apache2
+fi
